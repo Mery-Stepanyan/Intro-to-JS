4325a8fedb2ec2200eb3645544e6d0cf7ab06ff8
Mery-Stepanyan <64136735+Mery-Stepanyan@users.noreply.github.com>
2020-07-16 01:51:38 +0400

    The sum of numbers
    Sum

---
+++ index.js
@@ -0,0 +1,5 @@
+ 
+const firstNumber = prompt('Enter the first number');
+const secondNumber = prompt('Enter the second number');
+prompt('The sum is', parseInt(firstNumber) + parseInt(secondNumber));
+
